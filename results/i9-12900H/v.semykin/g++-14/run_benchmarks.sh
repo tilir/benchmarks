b54a6a84ef0e3e55f588e78f0aeee1db3fd260c4
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-09-11 13:01:42 +0300

    Uncommented all of the benchmarks executables

--- results/i9-12900H/v.semykin/g++-14/run_benchmarks.sh
+++ results/i9-12900H/v.semykin/g++-14/run_benchmarks.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 declare -A benchmarks=(
-    # ["coro-fibs"]="coro-fibs/coro-fibs"
-    # ["exc_ret"]="excret/exc_ret"
-    # ["exc_ret_noexc"]="excret/exc_ret_noexc"
-    # ["extswap"]="inline/extswap"
-    # ["exc_partition"]="noexcept-qsort/exc_partition"
-    # ["exc_qsort"]="noexcept-qsort/exc_qsort"
-    # ["filter"]="ranges-filter/filter"
-    # ["projector"]="ranges-projector/projector"
+    ["coro-fibs"]="coro-fibs/coro-fibs"
+    ["exc_ret"]="excret/exc_ret"
+    ["exc_ret_noexc"]="excret/exc_ret_noexc"
+    ["extswap"]="inline/extswap"
+    ["exc_partition"]="noexcept-qsort/exc_partition"
+    ["exc_qsort"]="noexcept-qsort/exc_qsort"
+    ["filter"]="ranges-filter/filter"
+    ["projector"]="ranges-projector/projector"
     ["virtinh"]="virtual-inherit/virtinh"
-    # ["virtinl"]="virtual-inline/virtinl"
-    # ["virtual"]="virtual-overhead/virtual"
-    # ["virtual-shuffle"]="virtual-overhead/virtual-shuffle"
+    ["virtinl"]="virtual-inline/virtinl"
+    ["virtual"]="virtual-overhead/virtual"
+    ["virtual-shuffle"]="virtual-overhead/virtual-shuffle"
 )
 
 base_dir="../../../../build/Release"
